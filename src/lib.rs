89174723bff450d0856a58122a010d555fef87bd
Kushagra Gupta <kushgpt08@gmail.com>
2020-08-10 15:35:36 +0530

    refactor testing

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,45 @@ pub mod sorts {
 }
 
 pub mod timer;
+
+use timer::time_it;
+
+macro_rules! once_iter {
+    ($sort: path, $name: expr, $arr: ident) => {
+        once(($name, time_it($sort, $arr.clone())))
+    };
+}
+
+pub fn test_on_all(arr: Vec<u32>) -> impl Iterator<Item = (&'static str, std::time::Duration)> {
+    use std::iter::once;
+    once_iter!(sorts::bubble_sort::bubble_sort, "Bubble Sort 1", arr)
+        .chain(once_iter!(
+            sorts::bubble_sort::bubble_sort_optimized,
+            "Bubble Sort 2",
+            arr
+        ))
+        .chain(once_iter!(sorts::cycle_sort::cycle_sort, "Cycle Sort", arr))
+        .chain(once_iter!(
+            sorts::insertion_sort::insertion_sort,
+            "Insertion Sort",
+            arr
+        ))
+        .chain(once_iter!(
+            sorts::selection_sort::selection_sort,
+            "Selection Sort",
+            arr
+        ))
+        .chain(once_iter!(sorts::shell_sort::shell_sort, "Shell Sort", arr))
+        .chain(once_iter!(
+            sorts::quicksort::basic_quicksort_median_pivot,
+            "QSort Median",
+            arr
+        ))
+        .chain(once_iter!(
+            sorts::quicksort::basic_quicksort_right_most_pivot,
+            "QSort Right",
+            arr
+        ))
+        .chain(once_iter!(sorts::heapsort::heapsort, "HeapSort", arr))
+        .chain(once_iter!(sorts::mergesort::mergesort, "MergeSort", arr))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,5 @@
-use sorting_benchmark::sorts;
-use sorting_benchmark::timer::time_it;
+use sorting_benchmark::test_on_all;
 use std::fs;
-
-macro_rules! time {
-    ($f: path, $name: expr, $arr: ident) => {
-        let t = time_it($f, $arr.clone());
-        println!(concat!($name, ":\t{:8} ms"), t.as_micros());
-    };
-}
-
-fn test_on_all(arr: Vec<u32>) {
-    time!(sorts::bubble_sort::bubble_sort, "Bubble Sort 1", arr);
-    time!(
-        sorts::bubble_sort::bubble_sort_optimized,
-        "Bubble Sort 2",
-        arr
-    );
-    time!(sorts::cycle_sort::cycle_sort, "Cycle Sort", arr);
-    time!(sorts::insertion_sort::insertion_sort, "Insertion Sort", arr);
-    time!(sorts::selection_sort::selection_sort, "Selection Sort", arr);
-    time!(sorts::shell_sort::shell_sort, "Shell Sort", arr);
-    time!(
-        sorts::quicksort::basic_quicksort_median_pivot,
-        "QSort Median",
-        arr
-    );
-    time!(
-        sorts::quicksort::basic_quicksort_right_most_pivot,
-        "QSort Right",
-        arr
-    );
-    time!(sorts::heapsort::heapsort, "HeapSort", arr);
-    time!(sorts::mergesort::mergesort, "MergeSort", arr);
-}
 
 macro_rules! parse_vec {
     ($name: literal) => {{
@@ -52,14 +19,22 @@ macro_rules! parse_vec {
 fn main() {
     println!("------------RANDOM------------");
     let arr = parse_vec!("random");
-    test_on_all(arr);
+    for (algo, time) in test_on_all(arr) {
+        println!("{}:\t{:8} ms", algo, time.as_micros());
+    }
     println!("------------DESCENDING------------");
     let arr = parse_vec!("descending");
-    test_on_all(arr);
+    for (algo, time) in test_on_all(arr) {
+        println!("{}:\t{:8} ms", algo, time.as_micros());
+    }
     println!("------------ASCENDING------------");
     let arr = parse_vec!("ascending");
-    test_on_all(arr);
+    for (algo, time) in test_on_all(arr) {
+        println!("{}:\t{:8} ms", algo, time.as_micros());
+    }
     println!("------------REPEATING------------");
     let arr = parse_vec!("repeating");
-    test_on_all(arr);
+    for (algo, time) in test_on_all(arr) {
+        println!("{}:\t{:8} ms", algo, time.as_micros());
+    }
 }
